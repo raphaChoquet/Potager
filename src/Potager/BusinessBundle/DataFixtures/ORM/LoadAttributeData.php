58e36afadab19742395984fe6f9cf4d95945b7ba
Bachelor 3 <bachelor3@iMac-StO-202-213.local>
2014-02-21 13:49:11 +0100

    Add Fixtures Attribute Data

---
+++ src/Potager/BusinessBundle/DataFixtures/ORM/LoadAttributeData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Potager\BusinessBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Potager\BusinessBundle\Entity\Attribute;
+
+class LoadAttributeData extends AbstractFixture implements OrderedFixtureInterface
+{
+	/**
+	* {@inheritDoc}
+	*/
+	public function load(ObjectManager $manager)
+	{
+		for($i = 0; $i < 10; $i++) {
+			$attribute = new Attribute();
+			$attribute->setLevel(1);
+			$attribute->setHp(100);
+			$attribute->setStrenght(5);
+			$attribute->setStamina(5);
+			$attribute->setAgility(5);
+			$attribute->setLuck(1);
+			$attribute->setUser($this->getReference('user-'.$i));
+			$manager->persist($attribute);
+
+		}
+		
+		$manager->flush();
+	}
+
+	/**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 3;
+    }
+}
