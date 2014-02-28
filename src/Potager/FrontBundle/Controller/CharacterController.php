b25884bda3c0962e62e9779565ce57a3e4bc7d4e
Narindra <Narindra@air-de-narindra.etudiant.saint-augustin.in.iesanetwork.com>
2014-02-28 17:21:43 +0100

    integration characters

--- src/Potager/FrontBundle/Controller/CharacterController.php
+++ src/Potager/FrontBundle/Controller/CharacterController.php
@@ -19,15 +19,10 @@ class CharacterController extends Controller
      */
     public function characterAction()
 	{
-	    $user1 = new Attribute();
-        $user2 = new Attribute();
+        $factions = $this->getDoctrine()
+        ->getRepository('PotagerBusinessBundle:Faction')
+        ->findAll();
 
-        $fight =  $this->get('potager_business.fight');
-
-        $timeForUser1 = $fight->attack($user1, $user2);
-        $timeForUser2 = $fight->attack($user1, $user2);
-
-        return array($timeForUser1 . ' :: ' . $timeForUser2);
-        
+        return array('factions'=>$factions);
 	}
 }
