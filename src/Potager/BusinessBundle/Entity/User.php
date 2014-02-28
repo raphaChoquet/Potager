bd3859e7b84e44f1eecd6e59140146b1925b3f42
raphaChoquet <rapha.choquet@gmail.com>
2014-02-28 12:23:36 +0100

    overide form registration

--- src/Potager/BusinessBundle/Entity/User.php
+++ src/Potager/BusinessBundle/Entity/User.php
@@ -4,7 +4,7 @@
 
 namespace Potager\BusinessBundle\Entity;
 
-use FOS\UserBundle\Entity\User as BaseUser;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 
