ce25ea75a639233a9863a6f13c3545cd43a4467e
Bachelor 3 <bachelor3@imac.etudiant.saint-augustin.in.iesanetwork.com>
2014-03-14 12:09:02 +0100

    create list users

---
+++ src/Potager/FrontBundle/Controller/ProfileController.php
@@ -0,0 +1,28 @@
+<?php
+namespace Potager\FrontBundle\Controller;
+
+use FOS\UserBundle\Controller\ProfileController as BaseController;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use FOS\UserBundle\Model\UserInterface;
+
+class ProfileController extends BaseController
+{
+    public function showAction(\Potager\BusinessBundle\Entity\User $user=null)
+    {
+        if( $user == null ) {
+            $user = $this->container->get('security.context')->getToken()->getUser();
+            if (!is_object($user) || !$user instanceof UserInterface) {
+                throw new AccessDeniedException('This user does not have access to this section.');
+            }
+        }
+         
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Profile:show.html.'.$this->container->getParameter('fos_user.template.engine'), array('user' => $user));
+
+    }
+
+}
